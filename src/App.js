c120ff153f9b37bd93e3b8b13daecc5413bb99f0
rcilomba <rcilomba@yahoo.com>
2024-10-26 00:34:18 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -289,7 +289,7 @@ function App() {
 
   return (
     <div className="container mx-auto p-5">
-      <h1 className="text-center text-4xl pb-10 ">Recept</h1>
+      <h1 className="text-center text-4xl pb-10 ">Ramma's Recept</h1>
       <div className="flex justify-center mb-5">
         <label htmlFor="search" className="mr-2">
           Sök:
